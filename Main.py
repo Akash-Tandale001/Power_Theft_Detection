37856cb5bdcf66dbc6219d67546b095c6c225882
Akash-Tandale001 <ak.tandale123@gmail.com>
2022-11-14 22:22:03 +0530

    graph update

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import Train
 import Visualisation1d
 import Visualisation2d
 
-DataProcessing.processData();
-Train.processTraining();
+#DataProcessing.processData();
+#Train.processTraining();
 Visualisation1d.visualisation1D();
 Visualisation2d.visualisation2D();
